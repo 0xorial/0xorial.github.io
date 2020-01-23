6c3af2e497241286e7541d7f1ef938c9158a3861
0xorial <0xorial@gmail.com>
2020-01-24 00:41:11 +0100

    updated spapp.

---
+++ spapp/precache-manifest.c405e458693647a3c6140857fae45bac.js
@@ -0,0 +1,52 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "url": "03ab771b196ad90f28a35a5aa8f5ec08.ttf"
+  },
+  {
+    "url": "053082015844707d8048e606b922cdad.ttf"
+  },
+  {
+    "url": "5c61819764f1fa973dda8ef77e391451.woff"
+  },
+  {
+    "url": "84c180eabdd692efd197345a33735e8c.woff"
+  },
+  {
+    "url": "9631870bf93f28bc1d7010bf55621ae8.eot"
+  },
+  {
+    "url": "ca7934b30a0b9829dbeb0b58a6fd0b51.eot"
+  },
+  {
+    "revision": "fe4bd06243b23ed26c8d91849ee4f924",
+    "url": "index.html"
+  },
+  {
+    "revision": "610ce693a990eb892225",
+    "url": "index.js"
+  },
+  {
+    "revision": "4ad1697470c09f6b55563bdb64f37a58",
+    "url": "public/environment.json"
+  },
+  {
+    "revision": "319d3c71333959b1f3ca7a32ed6531d3",
+    "url": "public/icon.png"
+  },
+  {
+    "revision": "639ee53e2475a96231510fa3dfbf9438",
+    "url": "public/index-dev.html"
+  },
+  {
+    "revision": "71d463beaeb6b3854e324c1452b1c5c4",
+    "url": "public/index.html"
+  },
+  {
+    "revision": "0dfa3219ce1d7e4006e4d8d447198141",
+    "url": "public/manifest.json"
+  },
+  {
+    "revision": "4b1d5e79f00e5946f8c0",
+    "url": "vendor.js"
+  }
+]);
---
+++ spapp/service-worker.js
@@ -0,0 +1,33 @@
+importScripts("precache-manifest.c405e458693647a3c6140857fae45bac.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets'
+  })
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30
+      })
+    ]
+  })
+);
+
+// Updating SW lifecycle to update the app after user triggered refresh
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
